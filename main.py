63cb713ac92c5ad660869f9435531e86e4dd102b
ISamoilenko <igorsam@gmail.com>
2023-04-15 18:36:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ from handlers import setup_dispatcher
 from settings import TELEGRAM_TOKEN, HEROKU_APP_NAME, PORT
 
 # Setup bot handlers
-updater = Updater(TELEGRAM_TOKEN)
+from settings import Bot
+myBot = Bot('TELEGRAM_TOKEN')
+updater = Updater(myBot)
+#updater = Updater(TELEGRAM_TOKEN)
 
 dp = updater.dispatcher
 dp = setup_dispatcher(dp)
